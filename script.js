8064b9252fd25af75c0d4d89fab6475a1c894195
unknown <you@example.com>
2022-08-17 22:27:32 +0600

    start about me part

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 //1rem=16px
-//20.17
+//1:34:00
 //https://youtu.be/lnV4drMyo-Q
+
+
+//typing script js
+let typed = new Typed(".typing",{
+    strings: ["web desaigner","you tuber","player","cooking specialist"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+let typed = new Typed(".typing-2",{
+    strings: ["web desaigner","you tuber","player","cooking specialist"],
+    typeSpeed: 1000,
+    backSpeed: 60,
+});
